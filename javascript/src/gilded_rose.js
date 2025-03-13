eb9a6b4ea85b24f46ecc806055094c60aafb9e5c
Deanna Stevens <deanna.stevens@atlantafalcons.nfl.com>
2025-03-12 20:11:20 -0400

    Add changes from pairing tech eval

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -5,29 +5,43 @@ class GildedRose {
     }
   }
 
-  processItemEndOfDay(item) {
-    if (item.name != 'Aged Cheddar' && item.name != 'Concert Tickets') {
-      if (item.quality > 0) {
-        if (item.name != 'Hammer') {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == 'Concert Tickets') {
-          if (item.daysRemaining < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.daysRemaining < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+  agedCheddar(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+    item.daysRemaining = item.daysRemaining - 1;
+    if (item.daysRemaining < 0 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+  }
+
+  concertTickets(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.name == 'Concert Tickets') {
+        if (item.daysRemaining < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
+        if (item.daysRemaining < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
       }
+    }
+    if (item.name != 'Hammer') {
+      item.daysRemaining = item.daysRemaining - 1;
+    }
+    if (item.daysRemaining < 0) {
+      item.quality = item.quality - item.quality;
+    }
+  }
+
+  updateRegularItem(item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
     }
     if (item.name != 'Hammer') {
       item.daysRemaining = item.daysRemaining - 1;
@@ -40,18 +54,24 @@ class GildedRose {
               item.quality = item.quality - 1;
             }
           }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
         }
       }
     }
   }
-}
 
+  processItemEndOfDay(item) {
+    if (item.name === 'Aged Cheddar') {
+      return this.agedCheddar(item);
+    }
+    if (item.name === 'Concert Tickets') {
+      return this.concertTickets(item);
+    }
+    if (item.name === 'Hammer') {
+      return
+    }
+    return this.updateRegularItem(item)
+  }
+}
 module.exports = {
   GildedRose,
 };
